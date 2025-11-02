59acdbc7207ba95180dc583afb658b35df595dcc
sid-methiya99 <siddharthmethiya180@gmail.com>
2025-11-03 00:24:49 +0530

    Added messages and fragment functionality

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,9 +1,8 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
-import { helloWorld } from "@/inngest/function";
+import { codingAgent } from "@/inngest/function";
 
-// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [helloWorld],
+  functions: [codingAgent],
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { toast } from "sonner";
 
@@ -10,8 +10,9 @@ export default function Home() {
   const [value, setValue] = useState("");
 
   const trpc = useTRPC();
-  const data = useMutation(
-    trpc.invoke.mutationOptions({
+  const { data: messages } = useQuery(trpc.messages.getMany.queryOptions());
+  const createMessage = useMutation(
+    trpc.messages.create.mutationOptions({
       onSuccess: () => {
         toast.success("Background job added");
       },
@@ -21,11 +22,12 @@ export default function Home() {
     <div className="mx-2 my-2 flex flex-col items-center justify-center">
       <Input value={value} onChange={(e) => setValue(e.target.value)} />
       <Button
-        disabled={data.isPending}
-        onClick={() => data.mutate({ value: value })}
+        disabled={createMessage.isPending}
+        onClick={() => createMessage.mutate({ value: value })}
       >
         Click to awake agent
       </Button>
+      {JSON.stringify(messages, null, 2)}
     </div>
   );
 }
